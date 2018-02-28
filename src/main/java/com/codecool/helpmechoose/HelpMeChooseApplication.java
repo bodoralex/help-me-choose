45e61e6e631d296d2a0bff27751ec37e60dbdf13
Alex Bodor <bodaleaa@gmail.com>
2018-02-28 21:30:05 +0100

    minor cleanup

--- src/main/java/com/codecool/helpmechoose/HelpMeChooseApplication.java
+++ src/main/java/com/codecool/helpmechoose/HelpMeChooseApplication.java
@@ -4,18 +4,9 @@ import com.codecool.helpmechoose.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.codecool.helpmechoose.repository")
-@EntityScan("com.codecool.helpmechoose.model")
 public class HelpMeChooseApplication {
-
-//	@Autowired
-//	DataSource dataSource;
 
 	@Autowired
     UserRepository userRepository;
